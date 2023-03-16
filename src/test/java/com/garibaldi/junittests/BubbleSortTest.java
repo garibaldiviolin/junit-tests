ebc1d9ea36230657bd8ab41b2076e767ac9a4132
neto <garibaldiviolin@gmail.com>
2023-03-15 21:03:42 -0300

    Fix test method name

--- src/test/java/com/garibaldi/junittests/BubbleSortTest.java
+++ src/test/java/com/garibaldi/junittests/BubbleSortTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class BubbleSortTest 
 {
     @Test
-    public void shouldOrderReversedList()
+    public void shouldSortReversedList()
     {
         int[] list = {3, 2, 1};
         BubbleSort bubbleSort = new BubbleSort(list);
